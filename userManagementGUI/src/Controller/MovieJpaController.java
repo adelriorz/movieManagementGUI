696e412412c07cf32614e9d4415fe0b0d6a45322
Armando Del Rio <adelriorz@gmail.com>
2022-11-24 21:52:48 -0800

    Crud + Search completed
    Crud + Search completed

--- userManagementGUI/src/Controller/MovieJpaController.java
+++ userManagementGUI/src/Controller/MovieJpaController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Controller;
 
 import Controller.exceptions.NonexistentEntityException;
@@ -17,7 +14,6 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
 /**
- *
  * @author Armand Riviere
  */
 public class MovieJpaController implements Serializable {
